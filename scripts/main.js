44b731d17e4682be03dc9468eecf8d4df32f69d4
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-12 09:55:58 -0500

    final commit

--- scripts/main.js
+++ scripts/main.js
@@ -9,13 +9,29 @@ const amount = prices.reduce(function(acc, i){
 
 let average = amount / prices.length;
 console.log(`The average price is $${average.toFixed(2)}`)
+// const prices = items
+    // .map(function(item){
+    //     return item.price;
+    // }); 
+    // .reduce(function(acc, i){
+    //     return acc + i;
+    // })
+//items.length
+
+// const priceOfAllItems = items.reduce(function(acc, i){
+//     return acc + i.price; //grabs the first object in the array and grab the first price in that object for the first time. will error the second time as acc.price isnt a prop 
+// }, 0) //puts zero as the value of acc the first time and then get i.price which is the nubmer and adds that together. 
+
 
 //Returns the objects of prices between $14-$18 
+// if (14 <= key.price && key.price <= 18){
+    //     return key;
+    // }
+
 const priceList = items.filter(function(key){
-    if (14 <= key.price && key.price <= 18){
-        return key;
-    }
+    return 14 <= key.price && key.price <= 18 && key.currency_code === "USD"
 })
+console.log(`Items that cost between $14.00 USD and $18.00 USD`)
 console.log(priceList)
 
 //Finds items with GBP code and returns the item and price
@@ -33,6 +49,14 @@ const curCodePrice = items.map(function(key){
 
 console.log(`${curCodTitle.join('')} cost £${curCodePrice.join('')}`);
 
+// items
+    // .filter(function(item){
+    //     return item.currency_cod === 'GBP'
+    // })
+    // .forEach(function(item){
+    //     console.log(`${item.title} costs ${item.price}`)
+    // });
+
 //Finds which items are made of wood
 const woodMat = items.filter(function(key){
     if(key.materials.includes('wood'))
@@ -42,6 +66,14 @@ const woodMat = items.filter(function(key){
 const woodTitle = woodMat.forEach(function(key){
     console.log(`${key.title} is made of wood`);
 })
+
+// items
+    // .filter(function(key){
+    //     return key.materials.includes('wood');
+    // })
+    // .forEach(function(key){
+    //     console.log(`${key.title} is made of wood`);
+    // })
 
 //Shows which items were made out of more then eight materials
 const larMat = items.filter(function(key){
@@ -58,7 +90,7 @@ larMat.forEach(function(key) {
 
 //Calculates how many items were made by sellers
 const selMade = items.filter(function(key){
-    return key.who_made == "i_did"
+    return key.who_made === "i_did"
 });
 
 console.log(`${selMade.length} were made by their sellers`)
